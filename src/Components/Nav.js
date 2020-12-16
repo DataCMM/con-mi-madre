0a09116f94448873673bd616f4bdb1abdce8fea3
monica-suarez <shutterbugmo@gmail.com>
2020-12-16 09:56:40 -0600

    added hover feature to nav buttons

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,31 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Nav = () => {
-	return (
-		<Links>
-			<List>
-				<Link to='/Landing'>Home</Link>
-				<Link to='/About'>About Us</Link>
-				<Link to='/Program'>Our Program</Link>
-				<Link to='/News'>News</Link>
-				<Link to='/Donate' id='donate'>
-					Donate
-				</Link>
-			</List>
-		</Links>
-	);
+  return (
+    <Links>
+      <List>
+        <Link to="/Landing" className="nav-button">
+          Home
+        </Link>
+        <Link to="/About" className="nav-button">
+          About Us
+        </Link>
+        <Link to="/Program" className="nav-button">
+          Our Program
+        </Link>
+        <Link to="/News" className="nav-button">
+          News
+        </Link>
+        <Link to="/Donate" id="donate">
+          Donate
+        </Link>
+      </List>
+    </Links>
+  );
 };
 
 export default Nav;
 
 const List = styled.ul`
-	display: flex;
-	justify-content: space-evenly;
-	padding-top: 2rem;
+  display: flex;
+  justify-content: space-evenly;
+  padding-top: 2rem;
 `;
 
 const Links = styled.nav`
-	height: 5vh;
+  height: 5vh;
 `;
