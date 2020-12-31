9bde23da29bf060a1ab1fe9fa32c9ae7fea8ec69
monica-suarez <shutterbugmo@gmail.com>
2020-12-31 17:23:14 -0600

    moved components into a universal header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,77 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaChevronDown } from "react-icons/fa";
+import { FiMenu } from "react-icons/fi";
 
-import Nav from "./Navigation/Nav";
-import CMMLogo from "../images/horizontal/color-and-white.png";
+// import Nav from "./Navigation/Nav";
 
 const Header = () => {
   return (
-    <div className="flex h-150 w-full">
-      <div className="h-full bg-opacity-0 md:w-1/4 flex flex-none">
-        <img src={CMMLogo} alt="Con mi madre logo" className="z-10" />
-      </div>
-      <div className="flex w-3/4 items-center justify-end pr-8">
-        <Nav />
+    <div>
+      <div className="h-13 w-screen relative flex flex-wrap items-center text-center justify-between navbar-expand-lg bg-gray-200 font-medium pl-2 pr-2">
+        <div className="hidden md:w-full md:flex md:flex-wrap md:items-center md:justify-between md:text-xl md:text-blue-500">
+          <div className="flex space-x-6 p-2 md: justify-start">
+            <Link className="hover:text-pink-500" to="/Landing">
+              HOME
+            </Link>
+            <Link className="hover:text-pink-500" to="Contact">
+              CONTACT US
+            </Link>
+          </div>
+          <div className="flex h-150 w-full">
+            <div className="flex w-3/4 items-center justify-end pr-8">
+              <div className="flex space-x-6 text-blue-500 text-base hidden md:contents text-md lg:text-md xl:text-lg">
+                <div className="flex flex-row items-center inline-block hover:text-pink-500">
+                  <Link to="/About" className="md:pr-2">
+                    About Us
+                  </Link>
+                  <FaChevronDown className="md:text-xs xl:text-sm" />
+                </div>
+                <div className="flex flex-row items-center inline-block hover:text-pink-500">
+                  <Link to="/Program" className="md:pr-2">
+                    Our Program
+                  </Link>
+                  <FaChevronDown className="md:text-xs xl:text-sm" />
+                </div>
+                <div className="flex flex-row items-center inline-block hover:text-pink-500">
+                  <Link to="/Community" className="md:pr-2">
+                    Our Community
+                  </Link>
+                  <FaChevronDown className="md:text-xs xl:text-sm" />
+                </div>
+                <div className="flex flex-row items-center inline-block hover:text-pink-500">
+                  <Link to="/Participant" className="md:pr-2">
+                    Participant Corner
+                  </Link>
+                  <FaChevronDown className="md:text-xs xl:text-sm" />
+                </div>
+                <Link
+                  to="/Donate"
+                  className="text-white px-2 rounded shadow-xl text-center text-gray-500 bg-pink-500 hover:text-pink-500 hover:bg-gray-500"
+                >
+                  Donate
+                </Link>
+              </div>
+            </div>
+          </div>
+          {/* <div className="flex md:flex-row justify-end text-center space-x-8 p-4 pr-6">
+          <Link className="hover:text-pink-500" to="/facebook">
+            <FaFacebookSquare className="md:text-3xl" />
+          </Link>
+          <Link className="hover:text-pink-500" to="/instagram">
+            <FaInstagram className="md:text-3xl" />
+          </Link>
+          <Link className="hover:text-pink-500" to="/twitter">
+            <FaTwitterSquare className="md:text-3xl" />
+          </Link>
+        </div> */}
+        </div>
+        <div className="p-4 mr-4 md:hidden">
+          <FiMenu />
+        </div>
       </div>
     </div>
   );
 };
+
 export default Header;
--- src/Components/Pages/About/Purpose.js
+++ src/Components/Pages/About/Purpose.js
@@ -1,117 +1,115 @@
-import React from 'react';
-import Social from '../../Navigation/Social';
+import React from "react";
 
 const Purpose = () => {
-	return (
-		<div>
-			<Social />
-			<div className='bg-yellow-500 flex justify-start content-start w-full h-1/3 bg-center bg-cover bg-no-repeat'>
-				<div className='text-gray-500 text-2xl font-bold tracking-widest pt-40 sm:text-xl md:text-4xl lg:text-5xl lg:pt-40'>
-					<h1>Our Purpose</h1>
-				</div>
-			</div>
-			{/* Future Section */}
-			<div className='flex flex-col p-4 gap-4 text-lg bg-blue-500 text-gray-500'>
-				<h2 className='text-xl'>LATINAS ARE THE FUTURE</h2>
-				<p>
-					In the next few years, the majority of all women in Texas will be
-					Latina.
-				</p>
-				<p>
-					Latinas are the most at risk for suffering from mental health issues
-					due to social, economic, political, cultural, and environmental
-					factors.
-				</p>
-			</div>
-			{/* Our Future */}
-			<div className='flex flex-col p-4 gap-4 text-lg bg-gray-500 text-blue-500'>
-				<h2 className='text-xl'>OUR HISTORY</h2>
-				<p>
-					The drive behind the founding of Con Mi MADRE stemmed from a statistic
-					that a Latina baby had less than a 1% chance of obtaining a college
-					education. Initially named the Hispanic Mother-Daughter Program (HMDP)
-					in 1992 as a project of the Junior League of Austin, the program set
-					out to create positive change in the lives of young Latinas by
-					empowering them to earn a college education. In 2008, the HDMP
-					transitioned into its own organization, adopting the name Con Mi
-					MADRE, becoming a freestanding and independent 501(c)(3) nonprofit
-					organization.
-				</p>
-				<p>
-					From inception, Con Mi MADRE’s focus has been on helping young Latinas
-					graduate from high school, enroll in college, and successfully earn a
-					college degree. We believe and understand the importance of parent
-					engagement in the educational process and intentionally engage mothers
-					in every aspect of our programming. To date, CMM has assisted over
-					3,000 mother-daughter teams on their journey to and through
-					post-secondary education. We are building our nation’s educated
-					workforce and the next generation of confident and diverse female
-					leaders. We will add text here
-				</p>
-			</div>
-			{/* Reports */}
-			<div className='flex flex-col p-4 gap-4 text-lg bg-blue-500 text-gray-500'>
-				<h2 className='text-xl'>CON MI MADRE REPORTS</h2>
-				<h3>Annual Reports</h3>
-				<ul>
-					<li>Links to PDFs</li>
-				</ul>
-				<h3>Statements</h3>
-				<ul>
-					<li>Links to PDFs</li>
-				</ul>
-			</div>
-			{/* Our Team */}
-			<div className='bg-gray-500'>
-				<div className='bg-yellow-500 h-48'>Our Team Banner Here</div>
-				<div>
-					<h3>Staff Pics with Bios</h3>
-				</div>
-				<div>
-					<h2>Board</h2>
-					<p>Executive Committee</p>
-					<p>Additional Board Members</p>
-				</div>
-			</div>
-			{/* Careers */}
-			<div className='bg-blue-500 text-gray-500 flex flex-col gap-4 p-4'>
-				<h2>Careers</h2>
-				<p>
-					Con Mi MADRE is looking for passionate, self-starters who want to
-					contribute to building our Nation's future Latina leaders and educated
-					workforce. With our five-pillars, unique curriculum and over 20 years
-					working with young Latinas and their mothers, we believe we have what
-					it takes to achieve our vision!
-				</p>
-				<p>
-					Please look at our current openings for opportunities to join our
-					familia!
-				</p>
-				<h2>Current Openings</h2>
-				<p>There are no openings at this time</p>
-				<h2>Interships</h2>
-				<h3>Programming Intern (Central Texas & Fort Worth chapters)</h3>
-				<p>
-					Con Mi MADRE offers opportunities for current social work students to
-					help with the implementation and execution of programming services.
-					Interested students must reach out to their School of Social Work
-					Field Office to initiate this process.
-				</p>
-				<h3>Communications Intern (Central Texas or Fort Worth chapter)</h3>
-				<p>
-					Con Mi MADRE is seeking a creative and dynamic student to assist in
-					marketing and communications efforts (part-time). Click here for more
-					information
-				</p>
-				<h3>Development Intern (Central Texas chapter)</h3>
-				<p>
-					Con Mi MADRE is seeking an organized and passionate student to assist
-					with fundraising, grants, and events (part-time). Click here for more
-					information
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="bg-yellow-500 flex justify-start content-start w-full h-1/3 bg-center bg-cover bg-no-repeat">
+        <div className="text-gray-500 text-2xl font-bold tracking-widest pt-40 sm:text-xl md:text-4xl lg:text-5xl lg:pt-40">
+          <h1>Our Purpose</h1>
+        </div>
+      </div>
+      {/* Future Section */}
+      <div className="flex flex-col p-4 gap-4 text-lg bg-blue-500 text-gray-500">
+        <h2 className="text-xl">LATINAS ARE THE FUTURE</h2>
+        <p>
+          In the next few years, the majority of all women in Texas will be
+          Latina.
+        </p>
+        <p>
+          Latinas are the most at risk for suffering from mental health issues
+          due to social, economic, political, cultural, and environmental
+          factors.
+        </p>
+      </div>
+      {/* Our Future */}
+      <div className="flex flex-col p-4 gap-4 text-lg bg-gray-500 text-blue-500">
+        <h2 className="text-xl">OUR HISTORY</h2>
+        <p>
+          The drive behind the founding of Con Mi MADRE stemmed from a statistic
+          that a Latina baby had less than a 1% chance of obtaining a college
+          education. Initially named the Hispanic Mother-Daughter Program (HMDP)
+          in 1992 as a project of the Junior League of Austin, the program set
+          out to create positive change in the lives of young Latinas by
+          empowering them to earn a college education. In 2008, the HDMP
+          transitioned into its own organization, adopting the name Con Mi
+          MADRE, becoming a freestanding and independent 501(c)(3) nonprofit
+          organization.
+        </p>
+        <p>
+          From inception, Con Mi MADRE’s focus has been on helping young Latinas
+          graduate from high school, enroll in college, and successfully earn a
+          college degree. We believe and understand the importance of parent
+          engagement in the educational process and intentionally engage mothers
+          in every aspect of our programming. To date, CMM has assisted over
+          3,000 mother-daughter teams on their journey to and through
+          post-secondary education. We are building our nation’s educated
+          workforce and the next generation of confident and diverse female
+          leaders. We will add text here
+        </p>
+      </div>
+      {/* Reports */}
+      <div className="flex flex-col p-4 gap-4 text-lg bg-blue-500 text-gray-500">
+        <h2 className="text-xl">CON MI MADRE REPORTS</h2>
+        <h3>Annual Reports</h3>
+        <ul>
+          <li>Links to PDFs</li>
+        </ul>
+        <h3>Statements</h3>
+        <ul>
+          <li>Links to PDFs</li>
+        </ul>
+      </div>
+      {/* Our Team */}
+      <div className="bg-gray-500">
+        <div className="bg-yellow-500 h-48">Our Team Banner Here</div>
+        <div>
+          <h3>Staff Pics with Bios</h3>
+        </div>
+        <div>
+          <h2>Board</h2>
+          <p>Executive Committee</p>
+          <p>Additional Board Members</p>
+        </div>
+      </div>
+      {/* Careers */}
+      <div className="bg-blue-500 text-gray-500 flex flex-col gap-4 p-4">
+        <h2>Careers</h2>
+        <p>
+          Con Mi MADRE is looking for passionate, self-starters who want to
+          contribute to building our Nation's future Latina leaders and educated
+          workforce. With our five-pillars, unique curriculum and over 20 years
+          working with young Latinas and their mothers, we believe we have what
+          it takes to achieve our vision!
+        </p>
+        <p>
+          Please look at our current openings for opportunities to join our
+          familia!
+        </p>
+        <h2>Current Openings</h2>
+        <p>There are no openings at this time</p>
+        <h2>Interships</h2>
+        <h3>Programming Intern (Central Texas & Fort Worth chapters)</h3>
+        <p>
+          Con Mi MADRE offers opportunities for current social work students to
+          help with the implementation and execution of programming services.
+          Interested students must reach out to their School of Social Work
+          Field Office to initiate this process.
+        </p>
+        <h3>Communications Intern (Central Texas or Fort Worth chapter)</h3>
+        <p>
+          Con Mi MADRE is seeking a creative and dynamic student to assist in
+          marketing and communications efforts (part-time). Click here for more
+          information
+        </p>
+        <h3>Development Intern (Central Texas chapter)</h3>
+        <p>
+          Con Mi MADRE is seeking an organized and passionate student to assist
+          with fundraising, grants, and events (part-time). Click here for more
+          information
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Purpose;
