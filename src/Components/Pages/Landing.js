13f197915a2a00bdef792bfd6be892b6cbdb7254
monica-suarez <shutterbugmo@gmail.com>
2020-12-31 16:20:57 -0600

    modified flex in mission section

--- src/Components/Pages/Landing.js
+++ src/Components/Pages/Landing.js
@@ -87,7 +87,7 @@ const Landing = () => {
             </p>
           </div>
         </div>
-        <div className="flex justify-center">
+        <div className="w-1/2 flex justify-center">
           <img id="grad" src={Graduate} alt="Female graduate" />
         </div>
       </div>
