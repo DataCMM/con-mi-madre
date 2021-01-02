c4ab19cd570dc8c9c2f61e3cc76f3c71452db1fc
monica-suarez <shutterbugmo@gmail.com>
2021-01-02 17:12:22 -0600

    included router file to clean up App component

--- src/App.js
+++ src/App.js
@@ -1,61 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Landing from "./Components/Pages/Landing";
-import Purpose from "./Components/Pages/About/Purpose";
-import Donate from "./Components/Pages/Donate";
-import Program from "./Components/Pages/Program/ProgrammingStructure";
-import Careers from "./Components/Pages/About/Careers";
-import Services from "./Components/Pages/Program/Services";
-import Involved from "./Components/Pages/Program/Involved";
-import Awards from "./Components/Pages/Community/Awards";
-import Events from "./Components/Pages/Community/Events";
-import Partners from "./Components/Pages/Community/Partners";
-import Resources from "./Components/Pages/Participants/Resources";
-import Contact from "./Components/Pages/About/Contact";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./Router";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/Resources">
-            <Resources />
-          </Route>
-          <Route path="/Contact">
-            <Contact />
-          </Route>
-          <Route path="/Partners">
-            <Partners />
-          </Route>
-          <Route path="/Awards">
-            <Awards />
-          </Route>
-          <Route path="/Events">
-            <Events />
-          </Route>
-          <Route path="/Involved">
-            <Involved />
-          </Route>
-          <Route path="/Services">
-            <Services />
-          </Route>
-          <Route path="/Careers">
-            <Careers />
-          </Route>
-          <Route path="/Purpose">
-            <Purpose />
-          </Route>
-          <Route path="/Donate">
-            <Donate />
-          </Route>
-          <Route path="/Program">
-            <Program />
-          </Route>
-          <Route path="/">
-            <Landing />
-          </Route>
-        </Switch>
-      </Router>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </div>
   );
 }
