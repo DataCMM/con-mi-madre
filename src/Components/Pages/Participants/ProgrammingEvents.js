35d0272da4d6dbba3fdfc821ab6146d3aca9ccef
monica-suarez <shutterbugmo@gmail.com>
2021-01-12 10:23:08 -0600

    added progr events component

---
+++ src/Components/Pages/Participants/ProgrammingEvents.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../Header";
+import Footer from "../../Footer";
+
+const ProgrammingEvents = () => {
+  return (
+    <div>
+      <Header />
+      <h3>Programming Events Here</h3>
+      <Footer />
+    </div>
+  );
+};
+
+export default ProgrammingEvents;
