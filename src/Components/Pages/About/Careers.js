f9412990282f3ee81a0c37f7f74c933a18ddd3c5
Gabe <jflores353@gmail.com>
2021-01-15 01:28:33 -0600

    Created Logo with only text to use on sub pages. Finished layoout for Careers page. Need to review with Monica.

--- src/Components/Pages/About/Careers.js
+++ src/Components/Pages/About/Careers.js
@@ -1,14 +1,98 @@
 import React from "react";
-import Header from "../../Header";
-import Footer from "../../Footer";
+
+import CareersBanner from "../../../images/careers/Careers-Banner.jpg";
+import Logo from "../../../images/stacked/text-only.png";
 
 const Careers = () => {
   return (
-    <div>
-      <Header />
-      <h1 className="page-body">Careers Page</h1>
-      <Footer />
-    </div>
+    <>
+      <div
+        style={{
+          backgroundImage: `url(${CareersBanner})`,
+        }}
+        className="w-full h-48 bg-cover bg-center bg-no-repeat"
+      >
+        <span
+          className="w-full h-48 absolute opacity-50 bg-black bg-cover"
+          id="overlay"
+        />
+        <div className="flex flex-col space-y-24">
+          <div className="sticky pt-4 pl-4 h-14 w-1/2">
+            <img src={Logo} alt="Con mi madre logo" className="z-10" />
+          </div>
+
+          <div className="sticky z-10 flex self-end pr-4 text-gray-500 text-3xl font-medium tracking-widest text-center">
+            <div id="title1">Careers</div>
+          </div>
+        </div>
+      </div>
+
+      {/* Careers Blue Content */}
+      <div className="bg-blue-500 text-gray-500 text-2xl shadow-xl h-auto w-full text-center p-6 pb-10 pt-10">
+        <p>
+          Con Mi MADRE is looking for passionate, self-starters who want to
+          contribute to building our Nation's future Latina leaders and educated
+          workforce. With our five-pillars, unique curriculum and over 20 years
+          working with young Latinas and their mothers, we believe we have what
+          it takes to achieve our vision!
+        </p>
+        <br />
+        <p>
+          Please look at our current openings for opportunities to join our
+          familia!
+        </p>
+      </div>
+
+      {/*  Openings */}
+      <div className="flex flex-col text-center pb-10 pt-10 bg-gray-500">
+        <h3 className="text-3xl text-pink-500">Current Openings</h3>
+        <p className="text-2xl">There are no openings at this time</p>
+      </div>
+
+      {/* Internships */}
+      <div className="bg-blue-500 text-gray-500 text-2xl shadow-xl h-auto w-full text-center p-6 pb-10 pt-10">
+        <h3 className="text-4xl underline">Internships</h3>
+        <br />
+        <div className="pb-6">
+          <h3 className="text-3xl">
+            Programming Intern (Central Texas & Fort Worth Chapter)
+          </h3>
+          <p className="text-2xl">
+            Con Mi MADRE offers opportunities for current social work students
+            to help with the implementation and execution of programming
+            services. Interested students must reach out to their School of
+            Social Work Field Office to initiate this process.
+          </p>
+        </div>
+        <div className="pb-6">
+          <h3 className="text-3xl">
+            Communications Intern (Central Texas or Fort Worth chapter)
+          </h3>
+          <p className="text-2xl">
+            Con Mi MADRE is seeking a creative and dynamic student to assist in
+            marketing and communications efforts (part-time). Click here for
+            more information
+          </p>
+        </div>
+        <div className="pb-6">
+          <h3 className="text-3xl">
+            Development Intern (Central Texas chapter)
+          </h3>
+          <p className="text-2xl">
+            Con Mi MADRE is seeking an organized and passionate student to
+            assist with fundraising, grants, and events (part-time). Click here
+            for more information
+          </p>
+        </div>
+      </div>
+
+      <div className="text-center p-6 pb-10 pt-10 text-orange-500 bg-gray-500">
+        <p className="text-2xl">
+          For more information on any job or internship opening, please reach
+          out to info@conmimadre.org.
+        </p>
+      </div>
+    </>
   );
 };
 
