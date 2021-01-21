2155ceae92f6efd591cf46f947cd060749087ba3
monica-suarez <shutterbugmo@gmail.com>
2021-01-21 13:47:45 -0600

    adjusting lg format, image issues

--- src/Components/Pages/About/Careers.js
+++ src/Components/Pages/About/Careers.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import CareersBanner from "../../../images/careers/Careers-Banner.jpg";
 import Intern from "../../../images/careers/Careers-1.jpg";
 
@@ -8,23 +7,25 @@ import Logo from "../../../images/stacked/text-only.png";
 const Careers = () => {
   return (
     <>
-      <div
-        style={{
-          backgroundImage: `url(${CareersBanner})`,
-        }}
-        className="w-full h-48 bg-cover bg-center bg-no-repeat"
-      >
-        <span
-          className="w-full h-48 absolute opacity-50 bg-black bg-cover"
-          id="overlay"
-        />
-        <div className="flex flex-col space-y-24">
-          <div className="sticky pt-4 pl-4 h-14 w-1/2">
-            <img src={Logo} alt="Con mi madre logo" className="z-10" />
-          </div>
+      <div id="careers-banner">
+        <div
+          style={{
+            backgroundImage: `url(${CareersBanner})`,
+          }}
+          className="w-full xs:h-48 bg-cover bg-fixed bg-center bg-no-repeat lg:h-full lg:pb-64"
+        >
+          <span
+            className="w-full xs:h-48 absolute opacity-50 bg-black bg-cover lg:h-full"
+            id="overlay"
+          />
+          <div className="flex flex-col space-y-24">
+            <div className="sticky pt-4 pl-4 h-14 w-1/2">
+              <img src={Logo} alt="Con mi madre logo" className="z-10" />
+            </div>
 
-          <div className="sticky z-10 flex self-end pr-4 text-gray-500 text-3xl font-medium tracking-widest text-center">
-            <div id="title1">Careers</div>
+            <div className="sticky z-10 flex self-end pr-4 text-gray-500 text-3xl font-medium tracking-widest text-center">
+              <div id="title1">Careers</div>
+            </div>
           </div>
         </div>
       </div>
