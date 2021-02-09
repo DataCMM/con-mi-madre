05101fb3ee119a49f84e5820fda333bd36d077a5
monica-suarez <shutterbugmo@gmail.com>
2021-02-09 16:08:55 -0600

    removed extra header and footer

--- src/Components/Pages/Participants/ProgrammingEvents.js
+++ src/Components/Pages/Participants/ProgrammingEvents.js
@@ -1,13 +1,9 @@
 import React from "react";
-import Header from "../../Header";
-import Footer from "../../Footer";
 
 const ProgrammingEvents = () => {
   return (
     <div>
-      <Header />
       <h3>Programming Events Here</h3>
-      <Footer />
     </div>
   );
 };
