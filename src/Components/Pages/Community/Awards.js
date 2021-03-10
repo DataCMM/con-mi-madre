2bbf95c1e2dc50a2c68419d0c29a2e5d3c1eab3b
monica-suarez <shutterbugmo@gmail.com>
2021-03-10 13:50:22 -0600

    awards banner done

--- src/Components/Pages/Community/Awards.js
+++ src/Components/Pages/Community/Awards.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Banner from "../../../images/awards/AwardsBanner.jpg";
+import AwardsBanner from "../../../images/awards/AwardsBanner.jpg";
 import Awards1 from "../../../images/awards/Awards-1.jpg";
 import Awards2 from "../../../images/awards/Awards-2.jpg";
 
@@ -9,7 +9,7 @@ import Logo from "../../../images/stacked/text-only.png";
 const Awards = () => {
   return (
     <>
-      <div
+      {/* <div
         style={{
           backgroundImage: `url(${Banner})`,
         }}
@@ -26,6 +26,29 @@ const Awards = () => {
 
           <div className="sticky z-10 flex self-end pr-4 text-gray-500 text-2xl font-medium tracking-widest text-center">
             <div id="title1">Awards & Recognition</div>
+          </div>
+        </div>
+      </div> */}
+      <div className="banner-image w-full h-full">
+        <div
+          style={{
+            backgroundImage: `url(${AwardsBanner})`,
+          }}
+          className="w-full h-full md:h-full relative  bg-cover bg-top bg-no-repeat"
+        >
+          <span
+            className="w-full h-full md:h-full absolute opacity-50 bg-black bg-cover"
+            id="career-overlay"
+          />
+
+          <div className="banner-text sticky flex flex-col justify-between">
+            <div className="sticky pt-4 pl-4 h-14 w-1/2">
+              <img src={Logo} alt="Con mi madre logo" className="z-10" />
+            </div>
+
+            <div className="career-title sticky z-10 flex self-end pr-4 pb-2 text-gray-500 text-4xl font-medium tracking-widest text-center">
+              <div id="title1">Awards & Recognition</div>
+            </div>
           </div>
         </div>
       </div>
