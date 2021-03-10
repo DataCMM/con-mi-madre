33757bdb0e897ae518c795bf8a818e6337a52fef
monica-suarez <shutterbugmo@gmail.com>
2021-03-10 13:55:40 -0600

    partners banner done

--- src/Components/Pages/Community/Partners.js
+++ src/Components/Pages/Community/Partners.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Banner from "../../../images/events/EventsBanner.jpg";
+import PartnersBanner from "../../../images/partners/OurPartnersBanner.jpg";
 import Logo from "../../../images/stacked/text-only.png";
 
 import CorporateGallery from "./CorporateGallery";
@@ -10,7 +10,7 @@ import "./community.css";
 const Partners = () => {
   return (
     <>
-      <div
+      {/* <div
         style={{
           backgroundImage: `url(${Banner})`,
         }}
@@ -27,6 +27,29 @@ const Partners = () => {
 
           <div className="sticky z-10 flex self-end pr-4 text-gray-500 text-2xl font-medium tracking-widest text-center">
             <div id="title1">Our Partners</div>
+          </div>
+        </div>
+      </div> */}
+      <div className="banner-image w-full h-full">
+        <div
+          style={{
+            backgroundImage: `url(${PartnersBanner})`,
+          }}
+          className="w-full h-full md:h-full relative  bg-cover bg-top bg-no-repeat"
+        >
+          <span
+            className="w-full h-full md:h-full absolute opacity-50 bg-black bg-cover"
+            id="career-overlay"
+          />
+
+          <div className="banner-text sticky flex flex-col justify-between">
+            <div className="sticky pt-4 pl-4 h-14 w-1/2">
+              <img src={Logo} alt="Con mi madre logo" className="z-10" />
+            </div>
+
+            <div className="career-title sticky z-10 flex self-end pr-4 pb-2 text-gray-500 text-4xl font-medium tracking-widest text-center">
+              <div id="title1">Our Partners</div>
+            </div>
           </div>
         </div>
       </div>
