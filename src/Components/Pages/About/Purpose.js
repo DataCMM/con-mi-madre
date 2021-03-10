e9158521fec9b1356efb39e73e367bda32afaf1a
monica-suarez <shutterbugmo@gmail.com>
2021-03-10 13:46:25 -0600

    purpose banner done

--- src/Components/Pages/About/Purpose.js
+++ src/Components/Pages/About/Purpose.js
@@ -8,7 +8,7 @@ import Logo from "../../../images/stacked/text-only.png";
 const Purpose = () => {
   return (
     <div>
-      <div className="h-48 w-full" id="purpose-banner">
+      {/* <div className="h-48 w-full" id="purpose-banner">
         <div
           style={{
             backgroundImage: `url(${PurposeBanner})`,
@@ -25,6 +25,29 @@ const Purpose = () => {
             </div>
 
             <div className="sticky z-10 flex self-end pr-4 text-gray-500 text-4xl font-medium tracking-widest text-center">
+              <div id="title1">Our Purpose</div>
+            </div>
+          </div>
+        </div>
+      </div> */}
+      <div className="banner-image w-full h-full">
+        <div
+          style={{
+            backgroundImage: `url(${PurposeBanner})`,
+          }}
+          className="w-full h-full md:h-full relative  bg-cover bg-top bg-no-repeat"
+        >
+          <span
+            className="w-full h-full md:h-full absolute opacity-50 bg-black bg-cover"
+            id="career-overlay"
+          />
+
+          <div className="banner-text sticky flex flex-col justify-between">
+            <div className="sticky pt-4 pl-4 h-14 w-1/2">
+              <img src={Logo} alt="Con mi madre logo" className="z-10" />
+            </div>
+
+            <div className="career-title sticky z-10 flex self-end pr-4 pb-2 text-gray-500 text-4xl font-medium tracking-widest text-center">
               <div id="title1">Our Purpose</div>
             </div>
           </div>
