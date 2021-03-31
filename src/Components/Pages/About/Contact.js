2a34901e5e7f9ecb69254eff45c22fb4a59142a1
Gabe <jflores353@gmail.com>
2021-03-31 14:13:40 -0500

    Contact page done

--- src/Components/Pages/About/Contact.js
+++ src/Components/Pages/About/Contact.js
@@ -2,66 +2,64 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <>
-      <form class="w-full max-w-lg m-auto p-6">
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+    <div className="bg-blue-500">
+      <form className="w-full max-w-lg m-auto p-6 bg-blue-500">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              className="block uppercase tracking-wide text-gray-500 text-sm font-bold mb-2"
               for="grid-first-name"
             >
-              First Name <p className="inline-block text-pink-500">*</p>
+              First Name <p className="inline-block text-black">*</p>
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
               id="grid-first-name"
               type="text"
             />
           </div>
-          <div class="w-full md:w-1/2 px-3">
+          <div className="w-full md:w-1/2 px-3">
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              className="block uppercase tracking-wide text-gray-500 text-sm font-bold mb-2"
               for="grid-last-name"
             >
-              Last Name <p className="inline-block text-pink-500">*</p>
+              Last Name <p className="inline-block text-black">*</p>
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-last-name"
               type="text"
-              placeholder="Doe"
             />
           </div>
         </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              className="block uppercase tracking-wide text-gray-500 text-sm font-bold mb-2"
               for="grid-password"
             >
-              E-mail <p className="inline-block text-pink-500">*</p>
+              E-mail <p className="inline-block text-black">*</p>
             </label>
             <input
-              class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="email"
               type="email"
-              placeholder="jane@gmail.com"
             />
           </div>
-          <p class="text-pink-500 pl-3 text-xs italic">
+          <p className="text-black pl-3 text-xs italic">
             * These fields are required.
           </p>
         </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3">
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full px-3">
             <label
-              class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              className="block uppercase tracking-wide text-gray-500 text-sm font-bold mb-2"
               for="grid-password"
             >
               Message
             </label>
             <textarea
-              class=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+              className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
               id="message"
               placeholder="We look forward to connecting with you!"
             ></textarea>
@@ -71,19 +69,19 @@ const Contact = () => {
             </p> */}
           </div>
         </div>
-        <div class="md:flex md:items-center">
-          <div class="md:w-1/3">
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3">
             <button
-              class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              className="shadow bg-pink-500 hover:bg-gray-500 hover:text-pink-500 focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded"
               type="button"
             >
               Send
             </button>
           </div>
-          <div class="md:w-2/3"></div>
+          <div className="md:w-2/3"></div>
         </div>
       </form>
-    </>
+    </div>
   );
 };
 
