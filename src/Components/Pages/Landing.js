4c5e43144355694df36b8ded2eb0773a4ac434ec
monica-suarez <shutterbugmo@gmail.com>
2021-04-05 10:33:25 -0500

    trying to check with image not fixed

--- src/Components/Pages/Landing.js
+++ src/Components/Pages/Landing.js
@@ -18,19 +18,19 @@ import ReactPlayer from "react-player";
 const Landing = () => {
   return (
     <div>
-      <div className="w-full h-auto">
+      <div className="w-full h-full">
         <div
           style={{
             backgroundImage: `url(${BannerImage})`,
           }}
-          className="w-full h-full relative bg-fixed bg-cover bg-top bg-no-repeat"
+          className="w-full h-full relative bg-cover bg-top bg-no-repeat"
         >
           <span
             className="w-full h-full absolute opacity-50 bg-black bg-cover"
             id="overlay"
           />
           {/* Banner contents  */}
-          <div className="flex flex-col">
+          <div className="sticky flex flex-col">
             <div className="flex flex-none pt-10 pb-24 pl-10 pr-10 h-36 w-auto xs:self-center md:h-1/4 md:w-1/4 md:self-start md:pl-10 bg-opacity-0">
               <img
                 src={CMMLogo}
