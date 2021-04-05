82b45c4524327bd05f15de35daac720fc1e82385
monica-suarez <shutterbugmo@gmail.com>
2021-04-05 16:46:07 -0500

    got all corporate sponsors up in a grid

--- src/Components/Pages/Community/Partners.js
+++ src/Components/Pages/Community/Partners.js
@@ -2,12 +2,20 @@ import React from "react";
 
 import PartnersBanner from "../../../images/partners/OurPartnersBanner.jpg";
 import Logo from "../../../images/stacked/text-only.png";
-
+import SetonLogo from "../../../images/coroporate-partners/Acension-Seton.jpg";
+import BOA from "../../../images/coroporate-partners/Bank-of-America-New-logo.jpg";
+import Bumble from "../../../images/coroporate-partners/bumble.jpg";
+import HEB from "../../../images/coroporate-partners/HEB_TOC_Star_Logo.jpg";
+import ABC from "../../../images/coroporate-partners/LOGO-ABC-BANK-5.jpg";
+import RBFCU from "../../../images/coroporate-partners/rbfcu.png";
+import TDS from "../../../images/coroporate-partners/TDSLogo.png";
+import TCB from "../../../images/coroporate-partners/TCB.jpg";
+import WF from "../../../images/coroporate-partners/WF.png";
 import "./community.css";
 
 const Partners = () => {
   return (
-    <>
+    <div className="bg-gray-500">
       <div className="banner-image w-full h-full">
         <div
           style={{
@@ -31,10 +39,54 @@ const Partners = () => {
           </div>
         </div>
       </div>
-      <div className="text-center text-gray-500 bg-blue-500 text-4xl font-semibold">
+      <div className="text-center bg-green-500 text-4xl font-semibold">
         Corporate Partners
       </div>
-    </>
+      <div className="bg-gray-500">
+        <div className="flex flex-col bg-white items-center grid grid-cols-2 gap-4 p-4 md:gap-8 lg:grid-cols-3 lg:p-8">
+          <a href="https://healthcare.ascension.org/" target="_blank">
+            <div
+              style={{ backgroundImage: `url(${SetonLogo})` }}
+              className="w-full h-48 bg-center bg-no-repeat  bg-contain"
+            />
+          </a>
+          <a href="https://www.bankofamerica.com/" target="_blank">
+            <div
+              style={{ backgroundImage: `url(${BOA})` }}
+              className="w-full h-48 bg-center bg-no-repeat bg-contain"
+            />
+          </a>
+          <div
+            style={{ backgroundImage: `url(${Bumble})` }}
+            className="w-full h-48 bg-center bg-no-repeat bg-contain"
+          />
+          <div
+            style={{ backgroundImage: `url(${HEB})` }}
+            className="w-full h-48 bg-center bg-no-repeat bg-contain"
+          />
+          <div
+            style={{ backgroundImage: `url(${ABC})` }}
+            className="w-full h-48 bg-center bg-no-repeat bg-contain"
+          />
+          <div
+            style={{ backgroundImage: `url(${RBFCU})` }}
+            className="w-full h-48 bg-center bg-no-repeat bg-contain"
+          />
+          <div
+            style={{ backgroundImage: `url(${TDS})` }}
+            className="w-full h-40 bg-center bg-no-repeat bg-contain"
+          />
+          <div
+            style={{ backgroundImage: `url(${TCB})` }}
+            className="w-full h-48 bg-center bg-no-repeat bg-contain"
+          />
+          <div
+            style={{ backgroundImage: `url(${WF})` }}
+            className="w-full h-48 bg-center bg-no-repeat bg-contain"
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
