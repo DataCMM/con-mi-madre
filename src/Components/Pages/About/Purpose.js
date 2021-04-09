9c32123db9dbf4263ccf867ed0ae751520f9eb00
monica-suarez <shutterbugmo@gmail.com>
2021-04-09 16:30:53 -0500

    changed padding for image in purpose

--- src/Components/Pages/About/Purpose.js
+++ src/Components/Pages/About/Purpose.js
@@ -72,7 +72,7 @@ const Purpose = () => {
         <div className="flex flex-col justify-center lg:items-end lg:w-1/2 pt-3">
           <img
             id="participants"
-            className="w-full h-auto p-6"
+            className="w-full h-auto lg:p-4 xl:p-8"
             src={Participants}
             alt="Program participants"
           />
