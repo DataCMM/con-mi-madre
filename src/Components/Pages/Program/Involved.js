e19e38e1f090311353c9b9f64dedc4c4c8e67760
monica-suarez <shutterbugmo@gmail.com>
2021-04-12 20:31:33 -0500

    created space for contact on involved page

--- src/Components/Pages/Program/Involved.js
+++ src/Components/Pages/Program/Involved.js
@@ -55,9 +55,12 @@ const Involved = () => {
         <h3 className="text-4xl font-semibold pb-6">
           Interested in Volunteering with Con Mi MADRE?
         </h3>
-        <p className="text-xl font-light pb-6">
+        <p className="text-xl font-light">
           Con Mi MADRE always has opportunities for individuals wishing to
-          contribute their time and talents to our mission! Please reach out to{" "}
+          contribute their time and talents to our mission!
+        </p>
+        <p className="text-xl font-light pb-6">
+          Please reach out to{" "}
           <a
             className="pt-2 font-medium hover:text-pink-500"
             href="mailto:info@conmimadre.org?subject=Mail%20from%20involved%20page"
