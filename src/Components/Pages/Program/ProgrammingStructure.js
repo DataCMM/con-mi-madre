a4840a5414a6a794ac9dde8fcdd82cfc1cd4e04d
monica-suarez <shutterbugmo@gmail.com>
2021-04-19 11:09:57 -0500

    base structure for progrm strc complete

--- src/Components/Pages/Program/ProgrammingStructure.js
+++ src/Components/Pages/Program/ProgrammingStructure.js
@@ -29,189 +29,80 @@ const Program = () => {
           </div>
         </div>
       </div>
-
-      {/* Preparedness */}
-      <div className="bg-blue-500 text-2xl text-gray-500 shadow-xl h-auto w-full text-center p-6 pb-6 pt-6">
-        <h3 className="text-3xl font-semibold pb-3">
-          Preparedness (6th - 10th Grades)
-        </h3>
-        <p className="pb-6">
-          We start early to prepare students for academic, social, and emotional
-          success. We host 12 group meetings throughout the school year where
-          our coordinators deliver our social-emotional education curriculum to
-          students at their campus.
-        </p>
-        <div className="h-full w-full">
-          <div
-            style={{
-              backgroundImage:
-                "url(/images/programming-structure/Preparedness-image.jpg)",
-            }}
-            className="intern-img bg-cover bg-top bg-no-repeat"
-          ></div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 bg-blue-500 text-gray-500 p-6">
+        <div className="flex flex-col bg-blue-500 text-gray-500 items-center text-center">
+          <img
+            src="/images/programming-structure/ProgramStructure1.jpg"
+            alt="Mentoring participants"
+            className="h-auto w-4/5 bg-cover bg-center p-6"
+          />
+          <div className="pr-6 pb-6 pl-6">
+            <h3 className="font-semibold text-2xl pb-3">
+              Preparedness Program (6th-10th Grades)
+            </h3>
+            <p className="font-light text-lg">
+              We start early to prepare students for academic, social, and
+              emotional success. We host various group meetings throughout the
+              school year where our coordinators deliver our social-emotional
+              education curriculum to students at their campus. The preparedness
+              program aims to build the student’s social-emotional skills while
+              guiding them in planning their college career.
+            </p>
+          </div>
         </div>
-        <p className="pt-6 pb-6">
-          Increase college knowledge, self-esteem, communication and coping
-          skills
-        </p>
-        <p className="pb-6">Encourage rigorous coursework</p>
-        <p className="pb-6">Commence career exploration</p>
+        <div className="flex flex-col bg-blue-500 text-gray-500  items-center text-center">
+          <img
+            src="/images/programming-structure/ProgramStructure2.jpg"
+            alt="Mentoring participants"
+            className="h-auto w-4/5 bg-cover bg-center p-6"
+          />
+          <div className="pr-6 pb-6 pl-6">
+            <h3 className="font-semibold text-2xl pb-3">
+              Participation Program (11th & 12th Grades)
+            </h3>
+            <p className="font-light text-lg">
+              Participants continue to receive social-emotional group meetings
+              while also commencing the college application process with close
+              guidance from a Con Mi Madre coordinator. The participation
+              program aims to make students college ready and emotionally
+              intelligent.
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col bg-blue-500 text-gray-500  items-center text-center">
+          <img
+            src="/images/programming-structure/ProgramStructure3.jpg"
+            alt="Mentoring participants"
+            className="h-auto w-4/5 bg-cover bg-center p-6"
+          />
+          <div className="pr-6 pb-6 pl-6">
+            <h3 className="font-semibold text-2xl pb-3">
+              Success Program (Undergraduate Students)
+            </h3>
+            <p className="font-light text-lg">
+              We continue to help Latinas reach their goal of a postsecondary
+              degree by offering students supportive guidance and academic
+              advising services. The Success Program offers multiple workshops
+              per year that cover career development, school belongingness, and
+              financial counseling. The success program aims to make students
+              career-ready and emotionally intelligent.
+            </p>
+          </div>
+        </div>
       </div>
-
-      {/* Participation */}
-      <div className="text-center p-6 pb-10 pt-10 bg-gray-500">
-        <h3 className="text-3xl font-semibold pb-3 text-black">
-          Participation (11th & 12th Grades)
+      <div className="bg-gray-500 p-6">
+        <h3 className="font-semibold text-2xl">
+          Programming Opportunities for Students and their Mothers Throughout
+          the Year
         </h3>
-        <p className="text-2xl pb-6">
-          Participants continue to receive social-emotional group meetings (8
-          per year) while learning college readiness basics. College Academies
-          are provided for those in the Participation Program where they
-          commence the college application process. Participation students are
-          also provided the opportunity to get paired with a Con Mi Hermana
-          mentor for additional one-on-one support.
-        </p>
-        <div
-          style={{
-            backgroundImage:
-              "url(/images/programming-structure/Participation-image.jpg)",
-          }}
-          className="intern-img bg-cover bg-center bg-no-repeat"
-        ></div>
-      </div>
-
-      {/* Success */}
-      <div className="bg-blue-500 text-2xl text-gray-500 shadow-xl h-auto w-full text-center p-6 pb-10 pt-10">
-        <h3 className="text-3xl font-semibold pb-3">
-          Success (Undergraduate Students)
-        </h3>
-        <p className="text-2xl pb-6">
-          We continue to help Latinas reach their goal of a postsecondary degree
-          by offering students supportive guidance and academic advising
-          services. The Success Program offers 6 workshops per year that cover
-          career development, school belongingness, and financial counseling and
-          a Success Symposium at the end of the year. Students are exposed to
-          scholarships and other professional development opportunities.
-        </p>
-        <div
-          style={{
-            backgroundImage:
-              "url(/images/programming-structure/Structure-3.jpg)",
-          }}
-          className="intern-img bg-cover bg-center bg-no-repeat"
-        ></div>
-      </div>
-      {/* Parent Program */}
-      <div className="text-center p-6 pb-10 pt-10 bg-gray-500">
-        <h3 className="text-3xl font-semibold pb-3 text-black">
-          Parent Program
-        </h3>
-        <p className="text-2xl pb-6">
-          Guardians who currently have their child in a Con Mi MADRE program are
-          encouraged to participate in the Parent Program. Parents can receive
-          case management support, and they are invited to various workshops
-          throughout the year where key players in the community are invited to
-          speak. The program seeks to prepare parents for their child’s
-          transition to college, expose them to effective parenting practices,
-          and empower them to set high goals for themselves and their families.
-        </p>
-        <div
-          style={{
-            backgroundImage:
-              "url(/images/programming-structure/Parent-Program-image.jpg)",
-          }}
-          className="intern-img bg-cover bg-top bg-no-repeat"
-        ></div>
-      </div>
-
-      {/* Additional Opportunities */}
-      <div className="bg-blue-500 text-2xl text-gray-500 shadow-xl h-auto w-full text-center p-6 pb-10 pt-10">
-        <h3 className="text-3xl pb-6 text-4xl font-bold">
-          Additional Opportunities
-        </h3>
-        <h3 className="text-3xl pt-6 pb-6 text-gray-500 font-semibold">
-          Conferences
-        </h3>
-        <div
-          style={{
-            backgroundImage:
-              "url(/images/programming-structure/Conferences-image.jpg)",
-          }}
-          className="intern-img bg-cover bg-center bg-no-repeat"
-        ></div>
-        <p className="text-2xl pt-6 pb-6">
-          Con Mi MADRE hosts a Balanced Living Conference every year for all
-          participants. This conference, led by passionate speakers from the
-          community, equips participants with the tools and strategies to
-          achieve emotional, physical, and financial wellbeing.{" "}
-        </p>
-        <h3 className="text-3xl pt-6 pb-6 text-gray-500 font-semibold">
-          College & Career Fair
-        </h3>
-        <div
-          style={{
-            backgroundImage:
-              "url(/images/programming-structure/College-and-Career-Fair-image.jpg)",
-          }}
-          className="intern-img bg-cover bg-center bg-no-repeat"
-        ></div>
-        <p className="text-2xl pt-6 pb-6">
-          Students from 6th-12th grades and their mothers are invited to attend
-          Con Mi MADRE’s Bilingual College & Career Fair. Mothers and daughters
-          are provided with the experience to speak to colleges, trade schools,
-          military representatives and meet with professionals to explore future
-          career options. The event is hosted in the Spring semester.
-        </p>
-        <h3 className="text-3xl pt-6 pb-6 text-gray-500 font-semibold">
-          College Visits
-        </h3>
-        <div
-          style={{
-            backgroundImage:
-              "url(/images/programming-structure/College-Visits-image.jpg)",
-          }}
-          className="intern-img bg-cover bg-center bg-no-repeat"
-        ></div>
-        <p className="text-2xl pt-6 pb-6">
-          Con Mi MADRE arranges College Visits two times per year for all
-          students 6th-12th grades and their mothers. These visits expose
-          participants to college campuses so that when the time comes for them
-          to make a decision about where to attend, they are able to make an
-          informed decision about the school that will meet the student’s needs.
-        </p>
-        <h3 className="text-3xl pt-6 pb-6 text-gray-500 font-semibold">
-          Leadership Summit
-        </h3>
-        <div
-          style={{
-            backgroundImage:
-              "url(/images/programming-structure/Leadership-summit-image.jpg)",
-          }}
-          className="intern-img bg-cover bg-center bg-no-repeat"
-        ></div>
-        <p className="text-2xl pt-6 pb-6">
-          Con Mi MADRE invites a distinguished group of students and their
-          mothers to the annual Leadership Summit. Participants hear and learn
-          from influential leaders, and they leave the summit with applicable
-          tools and resources to become future leaders.
-        </p>
-        <h3 className="text-3xl pt-6 pb-6 text-gray-500 font-semibold">
-          Volunteering
-        </h3>
-        <div
-          style={{
-            backgroundImage:
-              "url(/images/programming-structure/Volunteering-image.jpg)",
-          }}
-          className="intern-img bg-cover bg-center bg-no-repeat"
-        ></div>
-        <p className="text-2xl pt-6 pb-6">
-          Con Mi MADRE offers multiple volunteering opportunities for mothers
-          and daughters throughout the year. Participants are encouraged to
-          participate so they can accrue community service hours and experience
-          what it is like to make an impact in their community as a
-          mother-daughter team.
-        </p>
+        <ul>
+          <li>Balanced Living Conference</li>
+          <li>College Visits</li>
+          <li>College Fair</li>
+          <li>Leadership Summit</li>
+          <li>Volunteer Opportunities</li>
+          <li>Scholarship Opportunities</li>
+        </ul>
       </div>
     </>
   );
