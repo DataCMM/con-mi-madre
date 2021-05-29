a2fd66c4c597da470720db85786acf3a7790e33d
monica-suarez <shutterbugmo@gmail.com>
2021-05-29 12:49:54 -0500

    adjusting font size and padding for mobile

--- src/Components/Pages/Donate.js
+++ src/Components/Pages/Donate.js
@@ -42,45 +42,45 @@ const Donate = () => {
         </div>
       </div>
       <div className="flex flex-col p-6 md:p-12 gap-6 m-auto md:grid-cols-2 lg:grid-cols-3 bg-gray-500 text-center">
-        <div className="p-2">
+        <div className="p-6">
           <button className="rounded w-3/4 m-auto text-center p-5 h-32 bg-blue-500 text-gray-500">
-            <h3 className="pb-2 text-2xl font-semibold">$50</h3>
-            <p className="text-lg font-light">
+            <h3 className="md:pb-2 text-lg md:text-2xl font-semibold">$50</h3>
+            <p className="text-sm md:text-lg font-light">
               To buy school uniforms for scholars that need them.
             </p>
           </button>
         </div>
-        <div className="p-2">
+        <div className="p-6">
           <button className="rounded w-3/4 m-auto text-center p-5 h-36 bg-blue-500 text-gray-500">
-            <h3 className="pb-2 text-2xl font-semibold">$150</h3>
-            <p className="text-lg font-light">
+            <h3 className="md:pb-2 text-lg md:text-2xl font-semibold">$150</h3>
+            <p className="text-sm md:text-lg font-light">
               To purchase school supplies for every scholar to prevent the need
               to share.
             </p>
           </button>
         </div>
-        <div className="p-2">
+        <div className="p-6">
           <button className="rounded w-3/4 m-auto text-center p-5 h-36 bg-blue-500 text-gray-500">
-            <h3 className="pb-2 text-2xl font-semibold">$250</h3>
-            <p className="text-lg font-light">
+            <h3 className="md:pb-2 text-lg md:text-2xl font-semibold">$250</h3>
+            <p className="text-sm md:text-lg font-light">
               For personal protective equipment and cleaning supplies to keep
               everyone in the building safe.
             </p>
           </button>
         </div>
-        <div className="p-2">
+        <div className="p-6">
           <button className="rounded w-3/4 m-auto text-center p-5 h-36 bg-blue-500 text-gray-500">
-            <h3 className="pb-2 text-2xl font-semibold">$500</h3>
-            <p className="text-lg font-light">
+            <h3 className="md:pb-2 text-lg md:text-2xl font-semibold">$500</h3>
+            <p className="text-sm md:text-lg font-light">
               To purchase Chromebooks so that each scholar has a digital device
               to prevent educational gaps while learning at home
             </p>
           </button>
         </div>
-        <div className="p-2">
+        <div className="p-6">
           <button className="rounded w-3/4 m-auto text-center p-5 h-36 bg-blue-500 text-gray-500">
-            <h3 className="pb-2 text-2xl font-semibold">$1000</h3>
-            <p className="text-lg font-light">
+            <h3 className="md:pb-2 text-lg md:text-2xl font-semibold">$1000</h3>
+            <p className="text-sm md:text-lg font-light">
               For a Mi-Fi hotspot for a scholar to connect to hybrid classroom
               instruction.
             </p>
@@ -88,17 +88,19 @@ const Donate = () => {
         </div>
       </div>
       <div className="text-center m-auto bg-gray-500 p-6 pb-12">
-        <label className="text-2xl font-semibold">Enter Custom Amount</label>
+        <label className="text-xl md:text-2xl font-semibold">
+          Enter Custom Amount
+        </label>
         <div className="flex flex-row justify-center">
           <div className="p-3">
             <input
-              className="rounded text-center text-2xl font-semibold shadow-inner p-3"
+              className="rounded text-center text-lg md:text-2xl font-semibold shadow-inner p-3"
               placeholder="$0.00"
             />
           </div>
           <div className="p-3">
             <button className="rounded bg-blue-500 text-gray-500 p-3">
-              <h3 className="text-2xl font-light">Submit Custom Amount</h3>
+              <h3 className="text-lg md:text-2xl font-light">Donate</h3>
             </button>
           </div>
         </div>
