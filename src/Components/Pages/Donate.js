b15dfa02ea932341fe18aeec6808c5fab4fb7fa2
monica-suarez <shutterbugmo@gmail.com>
2021-05-29 12:32:44 -0500

    centering grid items

--- src/Components/Pages/Donate.js
+++ src/Components/Pages/Donate.js
@@ -41,43 +41,53 @@ const Donate = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col pt-10 pb-10 gap-6 md:grid-cols-2 lg:grid-cols-3 bg-gray-500 md:p-12">
-        <button className="rounded w-3/4 m-auto text-center p-5 h-32 bg-blue-500 text-gray-500">
-          <h3 className="pb-2 text-2xl font-semibold">$50</h3>
-          <p className="text-lg font-light">
-            To buy school uniforms for scholars that need them.
-          </p>
-        </button>
-        <button className="rounded w-3/4 m-auto text-center p-5 h-36 bg-blue-500 text-gray-500">
-          <h3 className="pb-2 text-2xl font-semibold">$150</h3>
-          <p className="text-lg font-light">
-            To purchase school supplies for every scholar to prevent the need to
-            share.
-          </p>
-        </button>
-        <button className="rounded w-3/4 m-auto text-center p-5 h-36 bg-blue-500 text-gray-500">
-          <h3 className="pb-2 text-2xl font-semibold">$250</h3>
-          <p className="text-lg font-light">
-            For personal protective equipment and cleaning supplies to keep
-            everyone in the building safe.
-          </p>
-        </button>
-        <button className="rounded w-3/4 m-auto text-center p-5 h-36 bg-blue-500 text-gray-500">
-          <h3 className="pb-2 text-2xl font-semibold">$500</h3>
-          <p className="text-lg font-light">
-            To purchase Chromebooks so that each scholar has a digital device to
-            prevent educational gaps while learning at home
-          </p>
-        </button>
-        <button className="rounded w-3/4 m-auto text-center p-5 h-36 bg-blue-500 text-gray-500">
-          <h3 className="pb-2 text-2xl font-semibold">$1000</h3>
-          <p className="text-lg font-light">
-            For a Mi-Fi hotspot for a scholar to connect to hybrid classroom
-            instruction.
-          </p>
-        </button>
+      <div className="flex flex-col p-6 md:p-12 gap-6 m-auto md:grid-cols-2 lg:grid-cols-3 bg-gray-500 text-center">
+        <div className="p-2">
+          <button className="rounded w-3/4 m-auto text-center p-5 h-32 bg-blue-500 text-gray-500">
+            <h3 className="pb-2 text-2xl font-semibold">$50</h3>
+            <p className="text-lg font-light">
+              To buy school uniforms for scholars that need them.
+            </p>
+          </button>
+        </div>
+        <div className="p-2">
+          <button className="rounded w-3/4 m-auto text-center p-5 h-36 bg-blue-500 text-gray-500">
+            <h3 className="pb-2 text-2xl font-semibold">$150</h3>
+            <p className="text-lg font-light">
+              To purchase school supplies for every scholar to prevent the need
+              to share.
+            </p>
+          </button>
+        </div>
+        <div className="p-2">
+          <button className="rounded w-3/4 m-auto text-center p-5 h-36 bg-blue-500 text-gray-500">
+            <h3 className="pb-2 text-2xl font-semibold">$250</h3>
+            <p className="text-lg font-light">
+              For personal protective equipment and cleaning supplies to keep
+              everyone in the building safe.
+            </p>
+          </button>
+        </div>
+        <div className="p-2">
+          <button className="rounded w-3/4 m-auto text-center p-5 h-36 bg-blue-500 text-gray-500">
+            <h3 className="pb-2 text-2xl font-semibold">$500</h3>
+            <p className="text-lg font-light">
+              To purchase Chromebooks so that each scholar has a digital device
+              to prevent educational gaps while learning at home
+            </p>
+          </button>
+        </div>
+        <div className="p-2">
+          <button className="rounded w-3/4 m-auto text-center p-5 h-36 bg-blue-500 text-gray-500">
+            <h3 className="pb-2 text-2xl font-semibold">$1000</h3>
+            <p className="text-lg font-light">
+              For a Mi-Fi hotspot for a scholar to connect to hybrid classroom
+              instruction.
+            </p>
+          </button>
+        </div>
       </div>
-      <div className="text-center bg-gray-500 p-6 pb-12">
+      <div className="text-center m-auto bg-gray-500 p-6 pb-12">
         <label className="text-2xl font-semibold">Enter Custom Amount</label>
         <div className="flex flex-row justify-center">
           <div className="p-3">
