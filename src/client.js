a79c2fbbd6e54efbac03f4fc71b9e52d91b4f49c
jflores353 <jflores353@gmail.com>
2021-06-14 15:32:17 -0500

    Used .env for Client.js

---
+++ src/client.js
@@ -0,0 +1,8 @@
+import sanityClient from '@sanity/client'
+
+export default sanityClient({
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    dataset: process.env.REACT_APP_API_DATASET,
+    apiVersion: process.env.REACT_APP_API_VERSION,
+    useCdn: process.env.REACT_APP_USE_CDN,
+})
