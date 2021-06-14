f15befa4f21270e272c039beca3ebc8d57f15a75
jflores353 <jflores353@gmail.com>
2021-06-14 15:51:29 -0500

    Made changes to .env credentials

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    dataset: process.env.REACT_APP_API_DATASET,
-    apiVersion: process.env.REACT_APP_API_VERSION,
-    useCdn: process.env.REACT_APP_USE_CDN,
+    projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+    dataset: `${process.env.REACT_APP_API_DATASET}`,
+    apiVersion: `2021-06-01`,
+    useCdn: `${process.env.REACT_APP_API_VERSION}`,
 })
