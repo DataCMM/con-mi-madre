a0c0d520fb881029fd59d1c2e2122e73eb0ac5df
monica-suarez <shutterbugmo@gmail.com>
2021-06-18 12:25:14 -0500

    trying to merge changes

--- src/Components/Pages/About/OurTeam.js
+++ src/Components/Pages/About/OurTeam.js
@@ -1,31 +1,30 @@
 import React, { useState, useEffect } from "react";
 import Logo from "../../../images/stacked/text-only.png";
-import Modal from "../../Modal"
+import Modal from "../../Modal";
 import "../pages.css";
 
-import sanityClient from "../../../client"
-import imageUrlBuilder from "@sanity/image-url"
+import sanityClient from "../../../client";
+import imageUrlBuilder from "@sanity/image-url";
 
-const builder = imageUrlBuilder(sanityClient)
+const builder = imageUrlBuilder(sanityClient);
 
-function urlFor(source){
-  return builder.image(source)
+function urlFor(source) {
+  return builder.image(source);
 }
 
 const OurTeam = () => {
-  const [ staff, setStaff ] = useState([])
-  const [ committee, setCommittee ] = useState([])
-  const [ board, setBoard ] = useState([])
+  const [staff, setStaff] = useState([]);
+  const [committee, setCommittee] = useState([]);
+  const [board, setBoard] = useState([]);
 
-  useEffect(()=>{
-    getStaff()
-    getCommittee()
-    getBoard()
-  }, [])
-  
-  async function getStaff (){
-      const data = await sanityClient
-      .fetch(`*[_type == "adminStaff"]{
+  useEffect(() => {
+    getStaff();
+    getCommittee();
+    getBoard();
+  }, []);
+
+  async function getStaff() {
+    const data = await sanityClient.fetch(`*[_type == "adminStaff"]{
         first_name,
         last_name,
         title,
@@ -38,28 +37,26 @@ const OurTeam = () => {
             url
           }
         }
-      }`)
-      setStaff(data)
+      }`);
+    setStaff(data);
   }
 
-  async function getCommittee (){
-    const data = await sanityClient
-    .fetch(`*[_type == "executiveCommittee"]{
+  async function getCommittee() {
+    const data = await sanityClient.fetch(`*[_type == "executiveCommittee"]{
      name,
      title,
      company_1,
      company_2
-    }`)
-    setCommittee(data)
+    }`);
+    setCommittee(data);
   }
- 
-  async function getBoard (){
-    const data = await sanityClient
-    .fetch(`*[_type == "additionalBoardMembers"]{
+
+  async function getBoard() {
+    const data = await sanityClient.fetch(`*[_type == "additionalBoardMembers"]{
     name,
     title
-    }`)
-    setBoard(data)
+    }`);
+    setBoard(data);
   }
 
   return (
@@ -90,59 +87,61 @@ const OurTeam = () => {
       </div>
       {/* Profiles with Images */}
       <div className="grid xs:grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 bg-gray-500 p-4 md:p-12">
-            className="w-48 md:w-1/2 h-48 mt-3 md:mt-0 bg-cover bg-top bg-no-repeat"
-          />
-          <div className="flex md:w-1/2 md:h-48 flex-col text-center md:text-left md:justify-between md:pl-5">
-            <div>
-              <h2 className="text-sm pt-4 md:pt-0">{member.title}</h2>
-              <h3 className="font-semibold text-md">{member.first_name.concat(member.last_name)}</h3>
-              <p className="pb-4 text-sm font-light">{member.pronoun}</p>
-            </div>
-            <div className="flex flex-col flex-wrap">
-               <Modal member={member} />
-               <a 
-                 className="pt-2 font-semibold text-sm hover:text-pink-500 hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                 href={member.email}
-               >
-                 Email {member.first_name}
-               </a>
-             </div>
-           </div> 
+        className="w-48 md:w-1/2 h-48 mt-3 md:mt-0 bg-cover bg-top bg-no-repeat"
+        />
+        <div className="flex md:w-1/2 md:h-48 flex-col text-center md:text-left md:justify-between md:pl-5">
+          <div>
+            <h2 className="text-sm pt-4 md:pt-0">{member.title}</h2>
+            <h3 className="font-semibold text-md">
+              {member.first_name.concat(member.last_name)}
+            </h3>
+            <p className="pb-4 text-sm font-light">{member.pronoun}</p>
+          </div>
+          <div className="flex flex-col flex-wrap">
+            <Modal member={member} />
+            <a
+              className="pt-2 font-semibold text-sm hover:text-pink-500 hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              href={member.email}
+            >
+              Email {member.first_name}
+            </a>
+          </div>
         </div>
-        ))}
       </div>
-      
       {/*  Executive Committee Members */}
-
       <div className="bg-green-500 text-xl m-auto text-center p-3 pb-6">
         <div>
           <h2 className="pt-3 font-extrabold text-4xl">Board Of Directors</h2>
           <h3 className="font-bold text-2xl pt-3 pb-2">Executive Committee</h3>
         </div>
 
-        {committee && committee.map((member, idx) => (
-          <div key={idx} className="pb-2" >
-          <h2>{member.name} - {member.title}</h2>
-          <h3>{member.company_1}</h3>
-          {member.company_2 ? <h3>{member.company_2}</h3> : null}
-        </div>
-        ))}  
-        
-       {/* Additonal Board Members */}
-        
+        {committee &&
+          committee.map((member, idx) => (
+            <div key={idx} className="pb-2">
+              <h2>
+                {member.name} - {member.title}
+              </h2>
+              <h3>{member.company_1}</h3>
+              {member.company_2 ? <h3>{member.company_2}</h3> : null}
+            </div>
+          ))}
+
+        {/* Additonal Board Members */}
+
         <div className="pb-3">
           <h2 className="font-bold text-2xl pt-3 pb-2">
             Additional Board Members
           </h2>
-          {board  && board.map((member,idx) => (
-            <div key={idx} className="pb-2">
-            <h2>{member.name}</h2>
-            <h3>{member.title}</h3>
-          </div>
-          ))}
+          {board &&
+            board.map((member, idx) => (
+              <div key={idx} className="pb-2">
+                <h2>{member.name}</h2>
+                <h3>{member.title}</h3>
+              </div>
+            ))}
         </div>
       </div>
-      {/* </div> */}
+      // {/* </div> */}
     </div>
   );
 };
