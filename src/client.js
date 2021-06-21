fb295a05cb2b26e87a0b318948bab44f8af73ca8
jflores353 <jflores353@gmail.com>
2021-06-21 16:20:27 -0500

    Added date back to Sanity client.

--- src/client.js
+++ src/client.js
@@ -3,6 +3,6 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
     projectId: `${process.env.REACT_APP_API_PROJECT_ID}`,
     dataset: `${process.env.REACT_APP_API_DATASET}`,
-    apiVersion: `${process.env.REACT_APP_API_VERSION}`,
+    apiVersion: `2021-06-01`,
     useCdn: `${process.env.REACT_APP_USE_CDN}`,
 })
