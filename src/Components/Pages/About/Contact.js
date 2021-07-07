651715c52cac5b7237c07a14da32ff8a26585c13
jflores353 <jflores353@gmail.com>
2021-07-07 17:04:47 -0500

    Added requirements for Netlify forms

--- src/Components/Pages/About/Contact.js
+++ src/Components/Pages/About/Contact.js
@@ -3,82 +3,90 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="bg-blue-500">
-      <form className="w-full max-w-lg m-auto p-6 bg-blue-500" netlify>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-500 text-sm font-bold mb-2"
-              for="grid-first-name"
-            >
-              First Name <p className="inline-block text-black">*</p>
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="grid-first-name"
-              type="text"
-            />
+      <form  
+        className="w-full max-w-lg m-auto p-6 bg-blue-500" 
+        name="contact" 
+        method="post"
+        >
+        <input type="hidden" name="form-name" value="contact" />
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-500 text-sm font-bold mb-2"
+                for="grid-first-name"
+              >
+                First Name <p className="inline-block text-black">*</p>
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                id="grid-first-name"
+                type="text"
+                name="first-name"
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-500 text-sm font-bold mb-2"
+                for="grid-last-name"
+                name="last-name"
+              >
+                Last Name <p className="inline-block text-black">*</p>
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="text"
+              />
+            </div>
           </div>
-          <div className="w-full md:w-1/2 px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-500 text-sm font-bold mb-2"
-              for="grid-last-name"
-            >
-              Last Name <p className="inline-block text-black">*</p>
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-last-name"
-              type="text"
-            />
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-500 text-sm font-bold mb-2"
+                for="grid-password"
+              >
+                E-mail <p className="inline-block text-black">*</p>
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="email"
+                type="email"
+                name="email"
+              />
+            </div>
+            <p className="text-black pl-3 text-xs italic">
+              * These fields are required.
+            </p>
           </div>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-500 text-sm font-bold mb-2"
-              for="grid-password"
-            >
-              E-mail <p className="inline-block text-black">*</p>
-            </label>
-            <input
-              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="email"
-              type="email"
-            />
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-500 text-sm font-bold mb-2"
+                for="grid-password"
+              >
+                Message
+              </label>
+              <textarea
+                className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+                id="message"
+                placeholder="We look forward to connecting with you!"
+                name="message"
+              ></textarea>
+              {/* <p class="text-gray-600 text-xs italic">
+                Re-size can be disabled by set by resize-none / resize-y /
+                resize-x / resize
+              </p> */}
+            </div>
           </div>
-          <p className="text-black pl-3 text-xs italic">
-            * These fields are required.
-          </p>
-        </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-gray-500 text-sm font-bold mb-2"
-              for="grid-password"
-            >
-              Message
-            </label>
-            <textarea
-              className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
-              id="message"
-              placeholder="We look forward to connecting with you!"
-            ></textarea>
-            {/* <p class="text-gray-600 text-xs italic">
-              Re-size can be disabled by set by resize-none / resize-y /
-              resize-x / resize
-            </p> */}
-          </div>
-        </div>
         <div className="md:flex md:items-center">
           <div className="md:w-1/3">
-            <button
-              className="shadow bg-pink-500 hover:bg-gray-500 hover:text-pink-500 focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded"
-              type="submit"
-            >
-              Send
-            </button>
+              <button
+                className="shadow bg-pink-500 hover:bg-gray-500 hover:text-pink-500 focus:shadow-outline focus:outline-none text-gray-500 font-bold py-2 px-4 rounded"
+                type="submit"
+              >
+                Send
+              </button>
           </div>
-          <div className="md:w-2/3"></div>
         </div>
       </form>
     </div>
