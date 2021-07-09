7ff6b8a9ac404a64ffe0a008aa212a423da04e76
jflores353 <jflores353@gmail.com>
2021-07-09 16:38:15 -0500

    Corrected semantics

--- src/Components/Pages/Participants/ProgrammingEvents.js
+++ src/Components/Pages/Participants/ProgrammingEvents.js
@@ -42,9 +42,9 @@ const ProgrammingEvents = () => {
   }
 
   return (
-    <div>
-      <div className="banner-image w-full h-full">
-        <div
+    <main>
+      <article className="banner-image w-full h-full">
+        <section
           style={{
             backgroundImage: "url(/images/team/OurTeamBanner.jpg)",
           }}
@@ -55,29 +55,29 @@ const ProgrammingEvents = () => {
             id="career-overlay"
           />
 
-          <div className="banner-text sticky flex flex-col justify-between">
+          <section className="banner-text sticky flex flex-col justify-between">
             <div className="sticky pt-4 pl-4 h-14 w-1/2">
               <img src={Logo} alt="Con mi madre logo" className="z-10" />
             </div>
             <div className="career-title sticky z-10 flex self-end pr-4 pb-2 text-white text-4xl font-medium tracking-widest text-center">
-              <div id="title1">Program Events</div>
+              <h1 id="title1">Program Events</h1>
             </div>
-          </div>
-        </div>
-      </div>
-      <div className="bg-gray-500">
+          </section>
+        </section>
+      </article>
+      <article className="bg-gray-500">
         <div className="bg-blue-500 text-gray-500">
-          <h1 className="text-2xl lg:text-3xl font-semibold pt-6 pr-6 pl-6 pb-2 text-center">
+          <h2 className="text-2xl lg:text-3xl font-semibold pt-6 pr-6 pl-6 pb-2 text-center">
             Calendar of Events for Participants
-          </h1>
-          <h1 className="text-xl lg:text-2xl font-semibold pr-6 pb-6 pl-6 text-center">
+          </h2>
+          <h2 className="text-xl lg:text-2xl font-semibold pr-6 pb-6 pl-6 text-center">
             Calendario de Eventos Para Participantes
-          </h1>
+          </h2>
         </div>
         <section>
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 p-12 text-center">
             <div>
-            <h1 className="text-2xl font-semibold pb-2">Fall/Otoño </h1>
+            <h2 className="text-2xl font-semibold pb-2">Fall/Otoño </h2>
             { fall && fall
               .sort((a, b) => a.id - b.id)
               .map((event,idx)=>(
@@ -92,7 +92,7 @@ const ProgrammingEvents = () => {
             }
             </div>
             <div>
-              <h1 className="text-2xl font-semibold pb-2">Spring/Primavera</h1>
+              <h2 className="text-2xl font-semibold pb-2">Spring/Primavera</h2>
               { spring && spring
               .sort((a, b) => a.id - b.id)
               .map((event,idx)=>(
@@ -132,8 +132,8 @@ const ProgrammingEvents = () => {
             className="h-auto w-full bg-cover bg-center"
           />
         </article>
-      </div>
-    </div>
+      </article>
+    </main>
   );
 };
 
